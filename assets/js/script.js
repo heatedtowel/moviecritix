6e7439c384272c4c4958a352d13b74723295e5c5
Jesse Monks <monksjesse@gmail.com>
2021-12-03 16:16:09 -0500

    giphy api logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,45 @@
-var searchEL = document.querySelector('')
-var randomEL = document.querySelector('')
-var movName = searchEL.value
-
-
+var movName = 'spider';
+var insGif = document.querySelector('#insGif')
 
 var omdbAPI = '2b938382b69dedd54e33dfae5ada1214'
 var giphAPI = 'nw35KvBZSmb4OKNw51Qur35t2fh1nqob'
 var omdbURL = 'https://gateway.marvel.com/v1/public/characters?name=' + movName + '&apikey=' + omdbAPI
-var giphURL = 'https://api.giphy.com/v1/gifs/random?api_key=' + giphAPI + '&tag=' + movName + '&rating=g'
+var giphURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphAPI + '&q=' + movName + '&limit=25&offset=0&rating=g&lang=en'
 
 
+
+
+
+
+init();
 
 
 function init() {
-  
+giphyCall();
 }
 
 
-
-
-
-function ombdCall() {
-
-fetch(omdbAPI).then(function (response) {
+function giphyCall() {
+fetch(giphURL).then(function (response) {
   return response.json();
 })
   .then(function (data) {
-    for (var item of data.results) {
+    var i = parseInt(Math.random() * data.data.length);
+      console.log(data.data)
+      gifResult = data.data[i].images.original.url;
+      console.log(gifResult)
 
-    }
+      insGif.setAttribute('src', gifResult)
   })
   
+  
 
-return omdbAPI.json
+return omdbAPI.json 
 };
 
-function giphCall() {
+function omdbCall() {
 
-  fetch(giphAPI).then(function (response) {
+  fetch(giphURL).then(function (response) {
     return response.json();
   })
     .then(function (data) {
